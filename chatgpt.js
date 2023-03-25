fdd477881bd2eef59031516982ccf2d5d4be3d69
eksido <45083828+eksido@users.noreply.github.com>
2023-03-25 12:34:53 +0100

    Bug fixes

--- chatgpt.js
+++ chatgpt.js
@@ -1,43 +1,62 @@
+const https = require("https");
+
 module.exports = function (RED) {
-    const axios = require("axios");
-  
-    function ChatGPTNode(config) {
-      RED.nodes.createNode(this, config);
-      const node = this;
-  
-      node.on("input", async (msg, send, done) => {
-        const apiKey = config.apiKey;
-        const prompt = msg.payload;
-        const apiUrl = "https://api.openai.com/v1/engines/text-davinci-003/completions";
-  
-        const payload = {
-          prompt: prompt,
-          max_tokens: config.maxTokens || 7,
-          n: config.numResponses || 1,
-          stop: config.stopSequences || "\n",
-          temperature: config.temperature || 0,
-          top_p: config.topP || 1,
-          stream: config.stream || false,
-          logprobs: config.logprobs || null,
-        };
-  
-        try {
-          const response = await axios.post(apiUrl, payload, {
-            headers: {
-              "Content-Type": "application/json",
-              "Authorization": 'Bearer ${apiKey}',
-            },
-          });
-  
-          msg.payload = response.data.choices.map((choice) => choice.text);
-          send(msg);
-        } catch (error) {
-          node.error("ChatGPT API request failed", error);
-        }
-  
+  function ChatGPTNode(config) {
+    RED.nodes.createNode(this, config);
+    const node = this;
+
+    node.on("input", async (msg, send, done) => {
+      const apiKey = config.apiKey;
+      const prompt = msg.payload;
+      const apiUrl = "https://api.openai.com/v1/engines/text-davinci-003/completions";
+
+      const payload = {
+        prompt: prompt,
+        max_tokens: config.maxTokens || 7,
+        n: config.numResponses || 1,
+        stop: config.stopSequences || "\n",
+        temperature: config.temperature || 0,
+        top_p: config.topP || 1,
+        stream: config.stream || false,
+        logprobs: config.logprobs || null,
+      };
+
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${apiKey}`,
+        },
+      };
+
+      const request = https.request(apiUrl, options, (response) => {
+        let data = "";
+
+        response.on("data", (chunk) => {
+          data += chunk;
+        });
+
+        response.on("end", () => {
+          if (response.statusCode === 200) {
+            const parsedData = JSON.parse(data);
+            msg.payload = parsedData.choices.map((choice) => choice.text);
+            send(msg);
+          } else {
+            node.error("ChatGPT API request failed: " + data);
+          }
+          done();
+        });
+      });
+
+      request.on("error", (error) => {
+        node.error("ChatGPT API request failed", error);
         done();
       });
-    }
-  
-    RED.nodes.registerType("chatgpt", ChatGPTNode);
-  };
+
+      request.write(JSON.stringify(payload));
+      request.end();
+    });
+  }
+
+  RED.nodes.registerType("chatgpt", ChatGPTNode);
+};
