67e6cd3173a21337f8df5ea755647ae8041101d2
eksido <45083828+eksido@users.noreply.github.com>
2023-03-28 18:21:01 +0200

    Chat node added

--- openai-chat.js
+++ openai-chat.js
@@ -1,5 +1,5 @@
 module.exports = function (RED) {
-    const axios = require("axios");
+    const https = require("https");
   
     function OpenAIChatNode(config) {
       RED.nodes.createNode(this, config);
@@ -8,31 +8,54 @@ module.exports = function (RED) {
       node.on("input", async (msg, send, done) => {
         const apiKey = config.apiKey;
         const messages = msg.payload;
-        const apiUrl = "https://api.openai.com/v1/chat/completions";
+        const apiUrl = new URL("/v1/chat/completions", "https://api.openai.com");
   
         const payload = {
-          model: config.model || "gpt-3.5-turbo",
+          model: "gpt-3.5-turbo",
           messages: messages,
         };
   
+        const options = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${apiKey}`,
+          },
+        };
+  
         try {
-          const response = await axios.post(apiUrl, payload, {
-            headers: {
-              "Content-Type": "application/json",
-              "Authorization": `Bearer ${apiKey}`,
-            },
+          const req = https.request(apiUrl, options, (res) => {
+            let data = "";
+  
+            res.on("data", (chunk) => {
+              data += chunk;
+            });
+  
+            res.on("end", () => {
+              if (res.statusCode >= 200 && res.statusCode < 300) {
+                const response = JSON.parse(data);
+                msg.payload = response.choices[0].message.content;
+                send(msg);
+              } else {
+                node.error(`Open AI Chat API request failed: ${data}`, msg);
+            }
+            done();
+            });
+            });
+            req.on("error", (error) => {
+                node.error("OpenAI Chat API request failed", error);
+                done();
+              });
+          
+              req.write(JSON.stringify(payload));
+              req.end();
+            } catch (error) {
+              node.error("OpenAI Chat API request failed", error);
+              done();
+            }
           });
-  
-          msg.payload = response.data.choices[0].message.text;
-          send(msg);
-        } catch (error) {
-          node.error("OpenAI Chat API request failed", error);
-        }
-  
-        done();
-      });
-    }
-  
-    RED.nodes.registerType("openai-chat", OpenAIChatNode);
-  };
-  
+          }
+          
+          RED.nodes.registerType("openai-chat", OpenAIChatNode);
+          };
+          
